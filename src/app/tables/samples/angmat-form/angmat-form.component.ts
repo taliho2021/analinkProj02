d61f95a2f679192224318d77fe0523a97d5cd526
Tal Yo <ty@ezams.com>
2021-08-13 05:45:03 -0400

    Change to angmat component

--- src/app/tables/samples/angmat-form/angmat-form.component.ts
+++ src/app/tables/samples/angmat-form/angmat-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { ThemeService } from 'ng2-charts';
+
 @Component({
   selector: 'app-angmat-form',
   templateUrl: './angmat-form.component.html',
@@ -10,36 +12,6 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AngmatFormComponent implements OnInit {
 
   importerForm!: FormGroup;
-  
-  // importerForm = this.fb.group({
-  //   clientId: [''],
-  //   name:  ['', Validators.required],
-  //   address1: ['', Validators.required],
-  //   address2: ['', Validators.required],
-  //   city: ['', Validators.required],
-  //   state: ['', Validators.required],
-  //   postalCode: [null, Validators.compose([
-  //     Validators.required, Validators.minLength(5), Validators.maxLength(5)])
-  //   ],
-  //   country: ['', Validators.required],
-  //   phone1:  [null, Validators.required],
-  //   email:  ['', Validators.required],
-  //   website: ['', Validators.required],
-  
-  //   htsGroup: this.fb.group({
-  //     htsNo: [],
-  //     description:[''],
-  //     spi:[''],
-  //     pga : this.fb.array([
-  //       this.fb.group({
-  //         pgaName:[''],
-  //         pgaCode:[''],
-  //         pgaMfr:[''],
-  //         pgaImp:['']
-  //       })
-  //     ])
-  //   })
-  // })
 
   constructor(private fb: FormBuilder) { }
 
@@ -52,6 +24,7 @@ export class AngmatFormComponent implements OnInit {
   importers(): FormArray{
     return this.importerForm.get('importers') as FormArray
   }
+
 
   newImporter(): FormGroup {
     return this.fb.group({
@@ -68,20 +41,14 @@ export class AngmatFormComponent implements OnInit {
         phone1:  [null, Validators.required],
         email:  ['', Validators.required],
         website: ['', Validators.required],
-      
+
         htsGroup: this.fb.group({
           htsNo: [],
           description:[''],
           spi:[''],
-          pga : this.fb.array([
-            this.fb.group({
-              pgaName:[''],
-              pgaCode:[''],
-              pgaMfr:[''],
-              pgaImp:['']
-            })
-          ])
-        })
+        }),
+
+        pgas: this.fb.array([])
     })
   }
 
@@ -93,8 +60,29 @@ export class AngmatFormComponent implements OnInit {
     this.importers().removeAt(impIndex)
   }
 
+  importerPga(impIndex:number): FormArray {
+    return this.importers().at(impIndex).get('pgas') as FormArray
+  }
+
+  newPga(): FormGroup{
+    return this.fb.group({
+      pgaName:[''],
+      pgaCode:[''],
+      pgaMfr:[''],
+      pgaImp:['']
+    })
+  }
+
+  addImporterPga(impIndex: number){
+    this.importerPga(impIndex).push(this.newPga())
+  }
+
+  removeImporterPga(impIndex:number, pgaIndex:number) {
+    this.importerPga(impIndex).removeAt(pgaIndex)
+  }
+
   onSubmit() {
     console.log(this.importerForm.value);
    }
-  
+
 }
