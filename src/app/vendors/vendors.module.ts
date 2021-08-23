51ee9e6dca471fc5196cfd464b86aafa5aae578a
taliho2021 <tyyo1980@gmail.com>
2021-08-23 15:07:12 -0400

    Created fifth branch

--- src/app/vendors/vendors.module.ts
+++ src/app/vendors/vendors.module.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -25,7 +26,8 @@ import { VendorsRoutingModule } from './vendors-routing.module';
     MatFormFieldModule,
     ReactiveFormsModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    FlexLayoutModule
   ]
 })
 export class VendorsModule { }
