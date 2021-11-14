fdca62198da5e5cb46f2cd2d3926b0ab00d99aca
Tal Yo <ty@ezams.com>
2021-11-14 06:51:46 -0500

    cb update

--- src/app/cb/cb.component.ts
+++ src/app/cb/cb.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-cb',
   templateUrl: './cb.component.html',
   styleUrls: ['./cb.component.scss']
 })
-export class CbComponent implements OnInit {
+export class CbComponent implements OnInit, OnDestroy {
 
+  private firstObsSubscription!: Subscription;
   constructor() { }
 
   ngOnInit(): void {
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+
+    // })
+
+    // Create a custom observable for interval fxn
+    const customIntervalObs = new Observable(observer =>{
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        // fake error to simulate HTTP call
+        if (count >9) {
+          observer.error(new Error('Count is greater than 9!'))
+        }
+        count++;
+      }, 1000)
+    })
+
+    this.firstObsSubscription = customIntervalObs.subscribe(data => {
+      console.log(data);
+
+    })
+  }
+
+  ngOnDestroy(): void{
+    this.firstObsSubscription.unsubscribe()
   }
 
 }
+
